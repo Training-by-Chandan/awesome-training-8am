64e7fee043007c49f28312ac3c5b2bbe00afb085
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-12-28 07:55:51 +0545

    casting explained

--- Console/ConsoleApp/ConsoleApp/Program.cs
+++ Console/ConsoleApp/ConsoleApp/Program.cs
@@ -10,13 +10,44 @@ namespace ConsoleApp
     {
         public static void Main()
         {
-            //Console.Write("Enter the name : ");
-            //string str = Console.ReadLine();
-            //Console.Clear();
+            //Add();
+            CastingExamples();
 
-            //Console.WriteLine("Hi " + str + "! Good morning");
-            Add();
             Console.ReadLine();
+        }
+
+        private static void CastingExamples()
+        {
+            {
+                char X = 'A';
+                X.ToString();
+            }
+            //X.ToString();// this is not possible because of scope
+
+            //implicit casting
+            //char => int => long => float => double
+            char c = 'A';//65 ASCII American Standard Code for Information Interchange
+            int i = c;
+            long l = i;
+            float f = l;
+            double d = f;
+
+            //explicit casting
+            double d1 = 1234.5677d;
+            float f1 = (float)d1;
+            long l1 = (long)f1;
+            int i1 = (int)f1; //Convert.ToInt32(f1);
+            char c1 = (char)i1;
+
+            //everything can be converted to string using '.ToString()'
+            string str = c.ToString();
+            string str1 = i.ToString();
+
+            //type conversion class (Convert.......)
+            string str3 = "1234.245245"; //64 bit floating point number
+            double d3 = Convert.ToDouble(str3);
+            int i2 = (int)Convert.ToDouble(str3);
+            decimal dec1 = Convert.ToDecimal(str3);
         }
 
         public static void Add()
