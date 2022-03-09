969c6b2ad88742b2a78c3065db9801809855582f
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-09 07:12:33 +0545

    raw sql migration

---
+++ Console/DbIntegration/CodeFirstIntegration/Migrations/202203090124164_ToAddView.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeFirstIntegration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class ToAddView : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("create view vw_studentView as \n\n Select * from Students");
+        }
+
+        public override void Down()
+        {
+            Sql("drop view vw_studentView");
+        }
+    }
+}
