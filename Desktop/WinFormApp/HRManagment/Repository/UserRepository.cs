28caa30c61ee4c197f53271aba72f5dbc8bee027
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-20 22:50:49 +0545

    separated repository

---
+++ Desktop/WinFormApp/HRManagment/Repository/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using HRManagment.Context;
+using HRManagment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagment.Repository
+{
+    public class UserRepository
+    {
+        private DefaultContext db = new DefaultContext();
+
+        public UserInfo GetByUserName(string username)
+        {
+            return db.UserInfos.FirstOrDefault(p => p.Username == username);
+        }
+
+        public (bool, string) CreateUser(UserInfo user)
+        {
+            try
+            {
+                db.UserInfos.Add(user);
+                db.SaveChanges();
+                return (true, "Added Successfully");
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public IQueryable<UserInfo> GetAllUSers()
+        {
+            return db.UserInfos.AsQueryable();
+        }
+    }
+}
