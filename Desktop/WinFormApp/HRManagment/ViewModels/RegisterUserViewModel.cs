5c16c844ca39e791726b6fa289189f1aa394609b
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-20 22:51:08 +0545

    added viewmodels

---
+++ Desktop/WinFormApp/HRManagment/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using HRManagment.Models;
+
+namespace HRManagment.ViewModels
+{
+    public class RegisterUserViewModel : LoginViewModel
+    {
+        public UserType UserType { get; set; }
+
+        public UserInfo ConvertToUserInfo()
+        {
+            return new UserInfo()
+            {
+                Username = this.Username,
+                Password = this.Password,
+                UserType = this.UserType
+            };
+        }
+    }
+}
---
+++ Desktop/WinFormApp/HRManagment/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HRManagment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagment.ViewModels
+{
+    public class UserInfoViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
