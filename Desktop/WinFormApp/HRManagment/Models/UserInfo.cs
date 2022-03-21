41e4b15d37f77b2c73e62f27c41985290b2d3b7e
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-21 23:25:36 +0545

    removed password field

--- Desktop/WinFormApp/HRManagment/Models/UserInfo.cs
+++ Desktop/WinFormApp/HRManagment/Models/UserInfo.cs
@@ -10,7 +10,6 @@ namespace HRManagment.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
         public UserType UserType { get; set; }
         public string PasswordHash { get; set; }
     }
