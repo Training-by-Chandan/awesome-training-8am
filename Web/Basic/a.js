5096d80633c1c4f918db2b15c9f92d734ad83931
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-04-05 22:01:35 +0545

    new themes added

--- Web/Basic/a.js
+++ Web/Basic/a.js
@@ -8,3 +8,29 @@ function slider()
     var x = document.getElementById("myRange").value;
     document.body.children[0].innerHTML = x;
 }
+//generate the random number
+function randomNumber()
+{
+    var x = Math.floor(Math.random() * 100);
+    document.getElementById('inpt').value = x;
+}
+
+//function to generate the fibonacci series
+function fibonacci()
+{
+    var x = document.getElementById('inpt').value;
+    var a = 0;
+    var b = 1;
+    var c = 0;
+    var fib = [];
+    fib[0] = a;
+    fib[1] = b;
+    for(var i = 2; i < x; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        fib[i] = c;
+    }
+    document.body.children[0].innerHTML = fib;
+}
---
+++ Web/WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Jeewsan()
+        {
+            return View();
+        }
+
+        public ActionResult Prabi(int page = 0)
+        {
+            if (page == 0)
+            {
+                return View("Jeewsan");
+            }
+            else
+            {
+                return View("Contact");
+            }
+        }
+
+        public ActionResult Display(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
