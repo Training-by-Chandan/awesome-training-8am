25ead8996373cf0d013fb3c9dfd45f08e6f1ba77
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-04-06 21:45:34 +0545

    Admin theme integration and partial page explained

---
+++ Web/WebApp/WebApp/ViewModels/PartialBoxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModels
+{
+    public class PartialBoxViewModel
+    {
+        public double Numbers { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+    }
+}
