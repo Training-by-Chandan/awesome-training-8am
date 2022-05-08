f3b31369ee8c7eea96060a39de8509b057ef5f48
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-05-08 21:32:26 +0545

    Integrated reports in mvc project

---
+++ Web/WebApp/Ecom.Web/Reports/ReportTemplate.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ecom.Web.Reports
+{
+    public partial class ReportTemplate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                rvSite.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Remote;
+                rvSite.ServerReport.ReportServerUrl = new Uri("https://localhost/ReportServer");
+                rvSite.ServerReport.ReportPath = "/EcomDb/" + Request["reportUrl"];
+                rvSite.ServerReport.Refresh();
+            }
+        }
+    }
+}
